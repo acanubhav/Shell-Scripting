e1f7a99b4fde46359eb447d5f4b4a2d4a3311178
acanubhav <acanubhav@gmail.com>
2014-10-03 18:44:37 +0530

    This kbkbThis is the scipt to find factorial of a numbrkber:Wqklkbwkrkr com

---
+++ s3.sh
@@ -0,0 +1,8 @@
+echo "enter the values for a and b"
+read a b
+echo the values of a is $a and the value of b is $b
+a=$[$a+$b]
+b=$[$a-$b]
+a=$[$a-$b]
+echo the values of a is $a and the value of b is $b
+
